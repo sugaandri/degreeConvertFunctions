643024bf529997344d242f182c9140ad38233e1c
Andri Suga <andrisuga@Andris-MacBook-Air.local>
2019-10-18 21:04:31 +0300

    f

--- DegreeConvert/Program.cs
+++ DegreeConvert/Program.cs
@@ -7,41 +7,54 @@ namespace DegreeConvert
         static void Main(string[] args)
         {
             string valik;
-            int a;
-            int x;
+            double a;
+            
+            
 
         Start:
             Console.WriteLine("Tere! See on Fahrenheit / Celsiuse kalkulaator.");
             Console.WriteLine("Kui soovid arvutada Fahrenheite vajuta F.");
             Console.WriteLine("Kui soovid arvutada Celsiuse'i vajuta C."); 
             valik = Console.ReadLine().ToLower();
+            Console.WriteLine("Nüüd sisestage kraadid: ");
+            a = int.Parse(Console.ReadLine());
             
 
            if (valik == "f")
             {
 
-                Console.WriteLine("Nüüd sisestage kraadid: ");
-                a = int.Parse(Console.ReadLine());
-                x = (a * 9) / 5 + 32;
-                Console.WriteLine($"Fahrenheit = {x}");
-                Console.WriteLine("Celsius = {a}");
+                Fahrenheit(a);
 
             }
 
            else if (valik == "c")
             {
-                Console.WriteLine("Nüüd sisestage kraadid: ");
-                a = int.Parse(Console.ReadLine());
-                x = (a - 32) * 5 / 9;
-                Console.WriteLine($"Celsius = {x}");
-                Console.WriteLine($"Fahrenheit = {a}");
+
+                Celsius(a);
             }
             else
             {
                 Console.WriteLine("Sellist valikut pole. Palun vali F või C");
                 goto Start;
             }
-            
+
+        }
+
+
+        public static void Fahrenheit(double kraad)
+        {
+
+            double x = Math.Round((kraad * 9) / 5 + 32, 1);
+            Console.WriteLine($"Fahrenheit: {x} "); //Kuidas lisada arvutus ja lause ühele reale?
+            Console.WriteLine($"Celsius: {kraad}");
+
+        }
+
+        public static void Celsius(double kraad)
+        {
+            double x = Math.Round((kraad - 32) * 5 / 9, 1);
+            Console.WriteLine($"Celsius:{x}");
+            Console.WriteLine($"Fahrenheit{kraad}");
         }
     }
 }
